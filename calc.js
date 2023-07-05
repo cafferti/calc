bc0f0ba73d5aab42b3fb4bebb03a25c0f84017ba
cafferti <saulkumven@gmail.com>
2023-07-05 11:43:15 +0100

    completed project

---
+++ calc.js
@@ -0,0 +1,30 @@
+
+    function addToResult(value) {
+      var resultDisplay = document.getElementById('resultDisplay');
+      var operationDisplay = document.getElementById('operationDisplay');
+      var currentOperation = operationDisplay.value;
+      
+      operationDisplay.value += value;
+      calculateResult();
+    }
+
+    function calculateResult() {
+      var resultDisplay = document.getElementById('resultDisplay');
+      var operationDisplay = document.getElementById('operationDisplay');
+      var currentOperation = operationDisplay.value;
+      
+      if (currentOperation !== '') {
+        var calculatedResult = eval(currentOperation);
+        resultDisplay.textContent = calculatedResult;
+      } else {
+        resultDisplay.textContent = '';
+      }
+    }
+    
+    function clearCalculator() {
+      var resultDisplay = document.getElementById('resultDisplay');
+      var operationDisplay = document.getElementById('operationDisplay');
+      
+      resultDisplay.textContent = '';
+      operationDisplay.value = '';
+    }
